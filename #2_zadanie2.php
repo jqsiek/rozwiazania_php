e73e34058f226aa3336780c7d19ca7c499e19822
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:45:29 +0200

    Create #2_zadanie2.php

---
+++ #2_zadanie2.php
@@ -0,0 +1,11 @@
+<?php
+/* USUN TEN KOMENTARZ
+Nazwa zmiennej jest dowolna - zamień na swoją nazwę
+Wartość zmiennej jest dowolna*/
+$zmienna = 13;
+if($zmienna%2 == 0 || $zmienna%3 == 0 || $zmienna%4 == 0 || $zmienna%5 == 0){
+  echo "TAK";
+}else{
+  echo "NIE";
+}
+?>
