0fd805dbc24fa408331d5117b962aec8aab638d4
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:40:34 +0200

    Create #2_zadanie1.php

---
+++ #2_zadanie1.php
@@ -0,0 +1,11 @@
+<?php
+/* USUN TEN KOMENTARZ
+Nazwa zmiennej jest dowolna - zamień na swoją nazwę 
+Zakladamy że 0 nie jest parzyste!*/
+$zmienna = 2;
+if($zmienna == 2 || $zmienna == 4 || $zmienna == 6 || $zmienna == 8){
+  echo "TAK";
+}else{
+  echo "NIE";
+}
+?>
