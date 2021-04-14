a38436beff840f285b256c7eb2b0b4ff025a9bed
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:42:58 +0200

    Update #2_zadanie1.php

--- #2_zadanie1.php
+++ #2_zadanie1.php
@@ -1,9 +1,10 @@
 <?php
 /* USUN TEN KOMENTARZ
-Nazwa zmiennej jest dowolna - zamień na swoją nazwę 
-Zakladamy że 0 nie jest parzyste!*/
+Nazwa zmiennej jest dowolna - zamień na swoją nazwę
+Wartość zmiennej jest dowolna z zakresu 0-9
+Zakladamy że 0 jest parzyste*/
 $zmienna = 2;
-if($zmienna == 2 || $zmienna == 4 || $zmienna == 6 || $zmienna == 8){
+if($zmienna == 0 || $zmienna == 2 || $zmienna == 4 || $zmienna == 6 || $zmienna == 8){
   echo "TAK";
 }else{
   echo "NIE";
