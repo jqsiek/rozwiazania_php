95a543938612f08e978fb9250b7d7d36a8242248
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:49:17 +0200

    Create #3_zadanie1 - sposob1.php

---
+++ #3_zadanie1 - sposob1.php
@@ -0,0 +1,9 @@
+<?php
+/* USUN TEN KOMENATRZ 
+tekst jest dowolny, zamień go na swój tekst, <br/> odpowiada za przejscie do kolejnej linii. */
+$a = 0;
+while($a < 10){
+  echo "tekst<br/>";
+  $a++;
+}
+?>
