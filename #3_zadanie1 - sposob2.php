54f316d1d540b3d18ad28bc8d5f7f0d1b9917ef8
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:50:23 +0200

    Create #3_zadanie1 - sposob2.php

---
+++ #3_zadanie1 - sposob2.php
@@ -0,0 +1,8 @@
+<?php
+/* USUN TEN KOMENATRZ 
+tekst jest dowolny, zamień go na swój tekst, <br/> odpowiada za przejscie do kolejnej linii. */
+for($a=0; $a<10; $a++){
+  echo "tekst<br/>";
+  $a++;
+}
+?>
