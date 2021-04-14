1911536f0331c39e70a88eac3f8c742f6659d768
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:28:23 +0200

    Create #1_zadanie1.php

---
+++ #1_zadanie1.php
@@ -0,0 +1,7 @@
+<?php
+/* USUN TEN KOMENTARZ
+Liczby przy zmienna2 można wybrać losowe, dlatego możesz zmienić na swoje, losowe. */
+$zmienna1 = 2*2;
+$zmienna2 = 4/2;
+$suma = $zmienna1 + $zmienna2;
+?>
