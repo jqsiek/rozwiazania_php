9e7d5f584802e42030a18addea33e08794ba8d68
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:36:49 +0200

    Delete #1_zadanie1.php

--- #1_zadanie1.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* USUN TEN KOMENTARZ
-Liczby przy zmienna2 można wybrać losowe, dlatego możesz zmienić na swoje, losowe. */
-$zmienna1 = 2*2;
-$zmienna2 = 4/2;
-$suma = $zmienna1 + $zmienna2;
-?>
