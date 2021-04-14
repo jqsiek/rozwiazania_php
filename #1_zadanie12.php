b24a9d4d3561e3bdeb76735a4807f3d49c71ff4d
Janek Gruszczyński <73364588+jqsiek@users.noreply.github.com>
2021-04-14 15:36:39 +0200

    Create #1_zadanie12.php

---
+++ #1_zadanie12.php
@@ -0,0 +1,9 @@
+<?php
+/* USUN TEN KOMENTARZ
+Liczby przy zmienna2 można wybrać losowe, dlatego możesz zmienić na swoje, losowe.
+To jest połączone zadanie 1 i 2 */
+$zmienna1 = 2*2;
+$zmienna2 = 4/2;
+$suma = $zmienna1 + $zmienna2;
+echo "Suma tych liczb to: ".$suma;
+?>
